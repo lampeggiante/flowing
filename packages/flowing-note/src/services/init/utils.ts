a56c0de12775ba8b6f3632279e71953533b2f8e0
Leon Yu <1099052496@qq.com>
2024-08-17 15:17:02 +0800

    fix: 加入了笔记父id

--- packages/flowing-note/src/services/init/utils.ts
+++ packages/flowing-note/src/services/init/utils.ts
@@ -3,20 +3,23 @@ import IndexedDBService from '../indexedDB'
 export function injectDBData(
   db: IndexedDBService,
   storeName: string,
-  data: any
+  data: any,
+  parent?: string | number
 ) {
   const { id, title, children } = data
   db.getStore(storeName, id).then((store) => {
-    if (store) return
-    db.addStore(storeName, {
+    const addData: Record<string, string | number | null> = {
       noteId: id,
       title,
-      content: '<p>这是内容</p>'
-    })
+      content: '<p>这是内容</p>',
+      parent: parent || null
+    }
+    if (store) return
+    db.addStore(storeName, addData)
   })
   if (children && children.length > 0) {
     children.forEach((item: any) => {
-      injectDBData(db, storeName, item)
+      injectDBData(db, storeName, item, id)
     })
   }
 }
---
+++ packages/flowing-note/src/utils/compare.ts
@@ -0,0 +1,23 @@
+export function compareObject(a: any, b: any): boolean {
+  const visited = new Set()
+  function compare(a: any, b: any): boolean {
+    if (a === b) return true
+    if (a === null || b === null) return false
+    if (typeof a !== 'object' || typeof b !== 'object') return false
+    if (typeof a === 'object' && typeof b === 'object') {
+      if (visited.has(a) || visited.has(b)) return true
+      visited.add(a)
+      visited.add(b)
+      const aKeys = Object.keys(a)
+      const bKeys = Object.keys(b)
+      if (aKeys.length !== bKeys.length) return false
+      return aKeys.every((key) => compare(a[key], b[key]))
+    }
+    if (Array.isArray(a) && Array.isArray(b)) {
+      if (a.length !== b.length) return false
+      return a.every((item, index) => compare(item, b[index]))
+    }
+    return a === b
+  }
+  return compare(a, b)
+}
