a693c1cb7635f094f79405a97c0d5294c2e3aa54
Leon Yu <1099052496@qq.com>
2024-08-21 17:50:38 +0800

    fix: 修复标题滞后更新的问题

--- packages/flowing-note/src/components/flowing-note-header/header-title/index.tsx
+++ packages/flowing-note/src/components/flowing-note-header/header-title/index.tsx
@@ -1,14 +1,13 @@
 import { FlowingTopHeading } from '@/components/flowing-editor'
 import { useNoteState } from '@/hooks/useNoteState'
 import { useNoteTree } from '@/hooks/useNoteTree'
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback } from 'react'
 import { useParams } from 'react-router-dom'
 
 export function HeaderTitle() {
   const { currentNote, setNoteTitle } = useNoteState()
   const { updateTreeItemTitle } = useNoteTree()
   const { id } = useParams()
-  const [title, setTitle] = useState(currentNote.noteTitle)
 
   const updateNoteTitle = useCallback(
     (title: string) => {
@@ -18,18 +17,10 @@ export function HeaderTitle() {
     [currentNote, updateTreeItemTitle, setNoteTitle]
   )
 
-  useEffect(() => {
-    if (id) {
-      setTitle(currentNote.noteTitle)
-    } else {
-      setTitle('Welcome to Flowing')
-    }
-  }, [id])
-
   return (
     <div className="app-header-title">
       <FlowingTopHeading
-        noteTitle={title}
+        noteTitle={currentNote.noteTitle}
         editable={Boolean(id)}
         setNoteTitle={updateNoteTitle}
       />
