95a8d2c5ca9ea82cc5f7d7340a8381b22fadbcad
Leon Yu <1099052496@qq.com>
2024-08-28 15:31:27 +0800

    feat: 完成tooltip基本布局

--- packages/flowing-components/index.ts
+++ packages/flowing-components/index.ts
@@ -1,1 +1,2 @@
 export { default as FlowingButton } from './src/flowing-button'
+export { default as FlowingTooltip } from './src/flowing-tooltip'
--- packages/flowing-components/src/flowing-tooltip/index.tsx
+++ packages/flowing-components/src/flowing-tooltip/index.tsx
@@ -1,1 +1,59 @@
-export {}
+import { cloneElement, useCallback, useEffect, useState } from 'react'
+import { createPortal } from 'react-dom'
+import { FlowingTooltipProps } from './types'
+
+const FlowingTooltip = (props: FlowingTooltipProps) => {
+  const {
+    children,
+    content,
+    placement = 'bottom',
+    tooltipCls = '',
+    disabled = false,
+    triggerType = 'hover',
+    arrowShow = true
+  } = props
+
+  const [trigger, setTrigger] = useState<HTMLElement | null>(null)
+  const [popover, setPopover] = useState<HTMLDivElement | null>(null)
+  const [show, setShow] = useState<boolean>(false)
+
+  const setTriggerDom = (node: HTMLElement) => {
+    if (node && node !== trigger) {
+      setTrigger(node)
+    }
+  }
+
+  const setPopoverDom = (node: HTMLDivElement) => {
+    if (node && node !== popover) {
+      setPopover(node)
+    }
+  }
+
+  const triggerDom = cloneElement(children, {
+    ref: setTriggerDom
+  })
+
+  const registerHoverEvent = useCallback(() => {}, [])
+
+  useEffect(() => {
+    if (triggerType === 'hover') {
+      registerHoverEvent()
+    }
+  }, [trigger, popover])
+
+  return (
+    <div className="flowing-tooltip">
+      <span className="flowing-tooltip-trigger">{triggerDom}</span>
+      {createPortal(
+        <div ref={setPopoverDom} className="" style={{}}>
+          {content}
+        </div>,
+        document.body
+      )}
+    </div>
+  )
+}
+
+FlowingTooltip.displayName = 'FlowingTooltip'
+
+export default FlowingTooltip
